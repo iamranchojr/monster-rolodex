fb3fcbaa82f80b0ab0533bcc8c2bc6534f5e045a
August Jr <iamranchojr@gmail.com>
2019-10-25 18:38:37 +0000

    Updates

---
+++ precache-manifest.25afed0c6f32f2bc87aed2c39e279fa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043de7903290dbee3884f0c7978619ef",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c177050faaec71a6e4b9",
+    "url": "/monster-rolodex/static/css/main.bb12e203.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monster-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "c177050faaec71a6e4b9",
+    "url": "/monster-rolodex/static/js/main.fcdcc321.chunk.js"
+  },
+  {
+    "revision": "e5540c1030566d31d8b0",
+    "url": "/monster-rolodex/static/js/runtime-main.bd5b8595.js"
+  }
+]);
